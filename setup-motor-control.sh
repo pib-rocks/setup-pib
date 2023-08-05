55a37317afbb9c13ec614d6615c576c0d97100a8
Juergen Weigert <jnweiger@gmail.com>
2023-08-05 23:05:51 +0200

    better explain re-

--- setup-motor-control.sh
+++ setup-motor-control.sh
@@ -24,16 +24,17 @@ source $source_cmd
 # ros2 run cerebra motor_control
 
 cat <<EOF
+Before using this service, edit 
+	~/motor_control_ws/src/cerebra/cerebra/motor_control.py
+Change UID, UID1, UID2 and UID3 of hat brick and bricklets retrieved from brickviewer (lines 3,4,5,6 in code)
+Then recompile with:		
+	( cd ~/motor_control_ws; colcon build --packages-select cerebra )
+
 To start this service, open a fresh shell, and do
 
 	$run_cmd
 
-Motor_control via cereba setup can be reached via http://localhost/head"
-Before using this service, edit 
-	~/motor_control_ws/src/cerebra/cerebra/motor_control.py
-Change UID, UID1, UID2 and UID3 of hat brick and bricklets retrieved from brickviewer (lines 3,4,5,6 in code)
-
-(Restart of the service may be needed after editing. See $0 )
+Motor_control via cereba setup can be reached at http://localhost/head"
 EOF
 
 
