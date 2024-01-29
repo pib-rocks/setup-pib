2bf59918b30e8f452c739a09fc41ce1467ad1dea
JaHe-isento <jan.hermann@isento.de>
2024-01-29 19:47:12 +0100

    feat(PR-573): Implement new user input parameter system

--- installation_scripts/check_user_input.sh
+++ installation_scripts/check_user_input.sh
@@ -1,46 +1,47 @@
 #!/bin/bash
 #
 # This script validates the user input arguments when calling the setup script
-# To properly run this script relies on being sourced by the "setup-pib.sh"-script
+# To properly run, this script relies on being sourced by the "setup-pib.sh"-script
 
-# Show infos about the command-line parameter options, then exit the script
+# The help function shows infos about the command-line parameter options, then exits the script
 help_function() 
 {
 	echo -e "Information about this script:"
 	echo -e "This script has two execution modes (normal mode and development mode).""$NEW_LINE"
 	echo -e "$YELLOW_TEXT_COLOR""To start the script in normal mode, don't add any arguments or options.""$RESET_TEXT_COLOR"
-	echo -e "Example: ./setup-pib""$NEW_LINE"
-	echo -e "$YELLOW_TEXT_COLOR""To start the script in development mode:""$RESET_TEXT_COLOR"
-	echo -e "- add the -d option after the script name."
-	echo -e "- specify the name of the default branch to be pulled as an argument"
-	echo -e "- specify the name of the feature branch as the third argument"
-	echo -e "$NEW_LINE""Example: ./setup-pib -d main PR-368"
-    echo -e "Example: ./setup-pib -d develop PR-368"
+	echo -e "Example: ./setup-pib""$NEW_LINE""$NEW_LINE"
+	echo -e "$YELLOW_TEXT_COLOR""Starting the script in development mode:""$RESET_TEXT_COLOR"
+	echo -e "Default branch parameter: -d=YourBranchName or --defaultBranch=YourBranchName"
+	echo -e "- This branch will be pulled if no feature branch was found or specified"
+	echo -e "Feature branch parameter: -f=YourBranchName or --featureBranch=YourBranchName"
+	echo -e "- If a branch with this name can be found in a repo, it will be pulled instead of the default branch"
+	echo -e "$NEW_LINE""Dev-mode example: ./setup-pib -d=main -f=PR-368"
+    echo -e "Another dev-mode example: ./setup-pib --defaultBranch=main --featureBranch=PR-368"
     exit "$INPUT_OUTPUT_ERROR_STATUS"
 }
 
 echo -e "$YELLOW_TEXT_COLOR""-- Checking possible user input options and arguments --""$RESET_TEXT_COLOR""$NEW_LINE"
 
-# Check if script was started in dev-mode (case insensitive comparison)
-if [ "$FIRST_USER_INPUT" = "-d" ] || [ "$FIRST_USER_INPUT" = "-D" ] || [ "$FIRST_USER_INPUT" = "dev" ] 
-then
-	is_dev_mode="$TRUE"
-
-	# Assign the second and thrid input argument to variables, if none are null
-	if [[ -n "$SECOND_USER_INPUT" ]] && [[ -n "$THIRD_USER_INPUT" ]]
-	then
-		user_default_branch="$SECOND_USER_INPUT"
-		user_feature_branch="$THIRD_USER_INPUT"
-	else
-		echo -e "$RED_TEXT_COLOR""Invalid argument syntax. Here is some info about the possible user inputs:""$RESET_TEXT_COLOR""$NEW_LINE"
-		help_function
-	fi
-
-# Show help if any other option was called
-elif [ -n "$FIRST_USER_INPUT" ] 
-then
-	echo -e "$RED_TEXT_COLOR""Invalid option inputs. Here is some info about the possible user inputs:""$RESET_TEXT_COLOR""$NEW_LINE"
-	help_function
-fi
+# Iterate through all user input parameters
+while [ $# -gt 0 ]; do
+	case "$1" in
+		# Assign default and feature branches for dev-mode
+		-d=* | --defaultBranch=*)
+			is_dev_mode="$TRUE"
+			user_default_branch="${1#*=}"
+			;;
+    	-f=* | --featureBranch=*)
+			is_dev_mode="$TRUE"
+			user_feature_branch="${1#*=}"
+			;;
+		-h | --help)
+			help_function
+			;;
+		*)
+			echo -e "$RED_TEXT_COLOR""Invalid option inputs. Here is some info about the possible user inputs:""$RESET_TEXT_COLOR""$NEW_LINE"
+			help_function
+	esac
+	shift
+done
 
 echo -e "$GREEN_TEXT_COLOR""-- User input option and argument syntax valid --""$RESET_TEXT_COLOR""$NEW_LINE"
